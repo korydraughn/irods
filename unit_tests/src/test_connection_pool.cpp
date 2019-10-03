2ee29eaa3d74e25780c5e48e7e285e40140befc2
Kory Draughn <korydraughn@renci.org>
2019-10-03 12:57:21 -0400

    [?] Connection Pool now accepts a custom client login function.

--- unit_tests/src/test_connection_pool.cpp
+++ unit_tests/src/test_connection_pool.cpp
@@ -1,9 +1,12 @@
 #include "catch.hpp"
 
 #include "getRodsEnv.h"
+#include "rcConnect.h"
 #include "connection_pool.hpp"
 #include "filesystem.hpp"
 #include "irods_at_scope_exit.hpp"
+
+#include <stdexcept>
 
 TEST_CASE("connection pool")
 {
@@ -56,6 +59,70 @@ TEST_CASE("connection pool")
         // Show that the released connection and the connection recently
         // created by the connection pool are indeed different connections.
         REQUIRE(released_conn_ptr != static_cast<rcComm_t*>(conn));
+    }
+
+    SECTION("custom login function")
+    {
+        bool called_custom_login_func = false;
+
+        SECTION("successful login")
+        {
+            const auto custom_login_func = [&](rcComm_t& _conn) -> void
+            {
+                called_custom_login_func = true;
+
+                if (clientLogin(&_conn) != 0) {
+                    throw std::runtime_error{"Login should have succeeded"};
+                }
+            };
+
+            REQUIRE_NOTHROW([&] {
+                const int cp_size = 1;
+                const int cp_refresh_time = 600;
+
+                irods::connection_pool conn_pool{cp_size,
+                                                 env.rodsHost,
+                                                 env.rodsPort,
+                                                 env.rodsUserName,
+                                                 env.rodsZone,
+                                                 cp_refresh_time,
+                                                 custom_login_func};
+
+                REQUIRE(called_custom_login_func);
+
+                namespace fs = irods::experimental::filesystem;
+
+                auto conn = conn_pool.get_connection();
+                REQUIRE(static_cast<rcComm_t*>(conn) != nullptr);
+                REQUIRE(fs::client::exists(conn, env.rodsHome));
+            }());
+        }
+
+        SECTION("failed login")
+        {
+            const char* expected_error_msg = "Thrown from login function!";
+
+            const auto custom_login_func = [&](rcComm_t& _conn) -> void
+            {
+                called_custom_login_func = true;
+                throw std::runtime_error{expected_error_msg};
+            };
+
+            REQUIRE_THROWS([&] {
+                const int cp_size = 1;
+                const int cp_refresh_time = 600;
+
+                irods::connection_pool conn_pool{cp_size,
+                                                 env.rodsHost,
+                                                 env.rodsPort,
+                                                 env.rodsUserName,
+                                                 env.rodsZone,
+                                                 cp_refresh_time,
+                                                 custom_login_func};
+            }(), expected_error_msg);
+
+            REQUIRE(called_custom_login_func);
+        }
     }
 }
 
