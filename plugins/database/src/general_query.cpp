b8b9d0499872316807c0f518172bbc59d48e3fb8
Kory Draughn <korydraughn@renci.org>
2021-01-11 18:58:02 -0500

    squash w/ 4887. Issue resolved. Needs a test now.

--- plugins/database/src/general_query.cpp
+++ plugins/database/src/general_query.cpp
@@ -101,6 +101,21 @@ int debug2 = 0;
 
 namespace
 {
+    // Transforms escaped GenQuery/SQL single quotes into actual single quotes (e.g. '' => ').
+    // If the range of characters defined by [_first, _last] contains adjacent single quotes,
+    // the range will be decreased by one and "_last" will be decremented by one.
+    void unescape_single_quotes(char* _first, char*& _last)
+    {
+        for (; _first < _last; ++_first) {
+            if ('\'' == *_first) {
+                if (auto* next_ch = _first + 1; next_ch < _last && *next_ch == '\'') {
+                    std::rotate(_first, _first + 1, _last);
+                    --_last;
+                }
+            }
+        }
+    }
+
     std::string_view::size_type find_unescaped_quote(const std::string_view _condition,
                                                      std::string_view::size_type _offset) noexcept
     {
@@ -1551,6 +1566,8 @@ insertWhere( char *condition, int option ) {
             }
         }
     }
+
+    unescape_single_quotes(cpFirstQuote, cpSecondQuote);
 
     if ( strcmp( condition, "IS NULL" ) == 0 ) {
         if ( !rstrcat( whereSQL, " ", MAX_SQL_SIZE_GQ ) ) { return USER_STRLEN_TOOLONG; }
