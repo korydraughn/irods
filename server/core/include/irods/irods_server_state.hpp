942343adec15f7a3c4a1a49dacc9606d5345ed36
Kory Draughn <korydraughn@renci.org>
2022-03-26 10:51:11 -0400

    [5249] Modernized server_state implementation.

--- server/core/include/irods/irods_server_state.hpp
+++ server/core/include/irods/irods_server_state.hpp
@@ -1,38 +1,36 @@
-
-
-
 #ifndef IRODS_SERVER_STATE_HPP
 #define IRODS_SERVER_STATE_HPP
 
 #include "irods/irods_error.hpp"
 
-#include <boost/thread/mutex.hpp>
-namespace irods {
-    class server_state {
-        public:
-            static server_state& instance();
-            error operator()( const std::string& _s );
-            std::string operator()();
+#include <string>
+#include <mutex>
 
-            static const std::string RUNNING;
-            static const std::string PAUSED;
-            static const std::string STOPPED;
-            static const std::string EXITED;
+namespace irods
+{
+    class server_state
+    {
+    public:
+        static const std::string RUNNING;
+        static const std::string PAUSED;
+        static const std::string STOPPED;
+        static const std::string EXITED;
 
-        private:
-            server_state();
-            server_state( server_state& ) {}
-            server_state( const server_state& ) {}
+        static server_state& instance();
 
-            boost::mutex mutex_;
-            std::string state_;
+        const std::string& operator()();
 
+        error operator()(const std::string& _new_state);
+
+    private:
+        server_state();
+        server_state(const server_state&) = delete;
+        server_state& operator=(const server_state&) = delete;
+
+        std::mutex mutex_;
+        std::string state_;
     }; // class server_state
-
-}; // namespace irods
-
-
+} // namespace irods
 
 #endif // IRODS_SERVER_STATE_HPP
-
 
--- server/core/src/irods_server_state.cpp
+++ server/core/src/irods_server_state.cpp
@@ -1,53 +1,50 @@
-
-
-#include "irods/rodsErrorTable.h"
 #include "irods/irods_server_state.hpp"
 
-namespace irods {
+#include "irods/rodsErrorTable.h"
 
-    const std::string server_state::RUNNING( "server_state_running" );
-    const std::string server_state::PAUSED( "server_state_paused" );
-    const std::string server_state::STOPPED( "server_state_stopped" );
-    const std::string server_state::EXITED( "server_state_exited" );
+namespace irods
+{
+    const std::string server_state::RUNNING = "server_state_running";
+    const std::string server_state::PAUSED  = "server_state_paused";
+    const std::string server_state::STOPPED = "server_state_stopped";
+    const std::string server_state::EXITED  = "server_state_exited";
 
-
-    server_state::server_state() :
-        state_( RUNNING )  {
-    }
-
-    server_state& server_state::instance() {
+    server_state& server_state::instance()
+    {
         static server_state instance_;
         return instance_;
     }
 
-    error server_state::operator()( const std::string& _s ) {
-        boost::mutex::scoped_lock lock( mutex_ );
-        if ( RUNNING != _s &&
-             PAUSED  != _s &&
-             STOPPED != _s &&
-             EXITED  != _s ) {
-            std::string msg( "invalid state [" );
-            msg += _s;
-            msg += "]";
-            return ERROR(
-                       SYS_INVALID_INPUT_PARAM,
-                       msg );
+    const std::string& server_state::operator()()
+    {
+        std::lock_guard lock{mutex_};
+        return state_;
+    }
 
+    error server_state::operator()(const std::string& _new_state)
+    {
+        std::lock_guard lock{mutex_};
+
+        if (RUNNING != _new_state &&
+            PAUSED  != _new_state &&
+            STOPPED != _new_state &&
+            EXITED  != _new_state)
+        {
+            std::string msg("invalid state [");
+            msg += _new_state;
+            msg += "]";
+            return ERROR(SYS_INVALID_INPUT_PARAM, msg);
         }
 
-        state_ = _s;
+        state_ = _new_state;
 
         return SUCCESS();
     }
 
-    std::string server_state::operator()() {
-        boost::mutex::scoped_lock lock( mutex_ );
-        return state_;
+    server_state::server_state()
+        : mutex_{}
+        , state_{RUNNING}
+    {
     }
-
-
-}; // namespace irods
-
-
-
+} // namespace irods
 
