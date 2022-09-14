d17695b74f7cb7a19ce217c8c1e7fee9a5bef1d3
Kory Draughn <korydraughn@renci.org>
2022-09-14 14:08:48 -0400

    [6588] Added feature test macros.
    C/C++ clients and server-side components now have a way to detect new
    features at compile-time. This allows implementations to compile/work
    across different versions of iRODS.

---
+++ lib/core/include/irods/library_features.h
@@ -0,0 +1,18 @@
+#ifndef IRODS_LIBRARY_FEATURES_H
+#define IRODS_LIBRARY_FEATURES_H
+
+/// \file
+/// \brief iRODS Feature Test Macros
+///
+/// This file defines macros that can be used to test whether one or more features
+/// are supported by the iRODS library.
+///
+/// \since 4.3.1
+
+/// Defined if the development library supports ::rc_switch_user.
+///
+/// \since 4.3.1
+#define IRODS_HAS_API_ENDPOINT_SWITCH_USER 202209L
+
+#endif // IRODS_LIBRARY_FEATURES_H
+
