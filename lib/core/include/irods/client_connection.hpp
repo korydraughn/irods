4f26987160ca2c3ad8bb5f96aae9096c7968a2df
Kory Draughn <korydraughn@renci.org>
2022-10-18 10:11:57 -0400

    [?] Fixed ODR violations in client_connection.hpp.

--- lib/core/include/irods/client_connection.hpp
+++ lib/core/include/irods/client_connection.hpp
@@ -18,13 +18,13 @@ namespace irods::experimental
     /// authentication occurs.
     ///
     /// \since 4.2.9
-    struct defer_connection {} defer_connection;
+    inline const struct defer_connection {} defer_connection;
 
     /// This move-only class provides a convenient way to connect to and
     /// disconnect from an iRODS server in a safe manner.
     ///
     /// \since 4.2.9
-    class client_connection
+    class client_connection // NOLINT(cppcoreguidelines-special-member-functions)
     {
     public:
         /// Connects and authenticates using the information and credentials
@@ -144,14 +144,14 @@ namespace irods::experimental
         /// \retval false Otherwise.
         ///
         /// \since 4.2.9
-        operator bool() const noexcept;
+        operator bool() const noexcept; // NOLINT(google-explicit-constructor)
 
         /// Returns a reference to the underlying connection.
         ///
         /// \throws irods::exception If the underlying connection is null.
         ///
         /// \since 4.2.9
-        operator RcComm&() const;
+        operator RcComm&() const; // NOLINT(google-explicit-constructor)
 
         /// Returns a pointer to the underlying connection.
         ///
