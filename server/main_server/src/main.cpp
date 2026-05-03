89f0e7e7e5c7ad1cc1cf1df508539e8b2f6d7767
Kory Draughn <korydraughn@renci.org>
2026-05-02 22:00:40 -0400

    squash w/ 8587. disallow running as root. remove code built on service_account.config

--- server/main_server/src/main.cpp
+++ server/main_server/src/main.cpp
@@ -136,7 +136,7 @@ namespace
     auto print_version_info() -> void;
 
     auto set_boot_time_as_environment_variable() -> void;
-    auto terminate_if_launched_by_incorrect_system_user() -> void;
+    auto terminate_if_launched_as_root() -> void;
     auto validate_configuration() -> bool;
     auto daemonize() -> void;
     auto create_pid_file(const std::string& _pid_file) -> int;
@@ -171,7 +171,7 @@ namespace
 auto main(int _argc, char* _argv[]) -> int
 {
     set_boot_time_as_environment_variable();
-    terminate_if_launched_by_incorrect_system_user();
+    terminate_if_launched_as_root();
 
     bool write_to_stdout = false;
     bool enable_test_mode = false;
@@ -255,6 +255,7 @@ auto main(int _argc, char* _argv[]) -> int
 
         init_logger(getpid(), write_to_stdout, enable_test_mode);
 
+        log_server::info("{}: real UID=[{}], real GID=[{}], effective UID=[{}], effective GID=[{}]", __func__, getuid(), getgid(), geteuid(), getegid());
         // Setting up signal handlers here removes the need for reacting to shutdown signals
         // such as SIGINT and SIGTERM during the startup sequence.
         if (setup_signal_handlers() == -1) {
@@ -469,80 +470,26 @@ Signals:
     } // set_boot_time_as_environment_variable
 
     // This function is meant to be called before the logger is initialized.
-    auto terminate_if_launched_by_incorrect_system_user() -> void
+    auto terminate_if_launched_as_root() -> void
     {
-        try {
-            const auto service_acct_file_path = irods::get_irods_config_directory() / "service_account.config";
-            std::ifstream in{service_acct_file_path};
-            if (!in) {
-                // A bad input stream signals that the user may not have the proper permissions to open
-                // the file.
-                fmt::print(stderr, "Error: Failed to open service account file [{}]. Cannot determine if user has permission to launch server. Exiting.\n", service_acct_file_path.c_str());
-                std::exit(1);
-            }
-
-            // Allocate a buffer large enough to hold the strings of a passwd struct.
-            auto buffer_size = sysconf(_SC_GETPW_R_SIZE_MAX);
-            if (buffer_size == -1) {
-                // Indeterminate value. Set the buffer to a value large enough to hold the strings
-                // of the passwd struct.
-                buffer_size = 16384;
-            }
-            auto buffer = std::make_unique<char[]>(buffer_size);
-
-            // The prefix identifying the service account user.
-            constexpr std::string_view key = "IRODS_SERVICE_ACCOUNT_NAME=";
-
-            // Read the file line by line until the service account user is found. The file is expected to
-            // contain exactly one line containing the information of interest.
-            std::string line;
-            while (in && std::getline(in, line)) {
-                if (!line.starts_with(key)) {
-                    continue;
-                }
-
-                // Throws if the starting position is greater than the size of the string.
-                const auto value = line.substr(key.size());
-
-                // Lookup the service account user's information. This information comes from the system.
-                passwd pwd; // NOLINT(cppcoreguidelines-pro-type-member-init)
-                passwd* result{};
-                const auto ec = getpwnam_r(value.c_str(), &pwd, buffer.get(), buffer_size, &result);
-                if (nullptr == result) {
-                    if (ec == 0) {
-                        fmt::print(stderr, "Warning: Service account user [{}] not found in system. Exiting.\n", value);
-                    }
-                    else {
-                        fmt::print(stderr, "Error: Could not retrieve information for user [{}] due to a system error: errno=[{}]. Exiting.\n", value, ec);
-                    }
-                    std::exit(1);
-                }
-
-                // Compare the effective UID of the server process with the UID of the service account user.
-                // Terminate if they are not identical.
-                if (const auto euid = geteuid(); euid != pwd.pw_uid) {
-                    fmt::print(
-                        stderr,
-                        "Warning: UID [{}] of server process does not match UID [{}] of service account user [{}]. Exiting.\n",
-                        euid,
-                        pwd.pw_uid,
-                        value);
-                    std::exit(1);
-                }
-
-                // The user who is attempting to launch the server matches the system user defined in the
-                // file. Return immediately since there's no more work to do.
-                return;
-            }
-
-            fmt::print(stderr, "Error: Could not find service account user in file [{}]. User not allowed to launch server. Exiting.\n", service_acct_file_path.c_str());
+        if (geteuid() == 0) {
+            fmt::print(stderr, "Warning: Launching server as with root privileges is not allowed. Exiting.\n");
             std::exit(1);
         }
-        catch (const std::exception& e) {
-            fmt::print(stderr, "Error: Caught exception while verifying the user's permission to launch server: {}\n", e.what());
+
+        // Disallow setuid-related security vulnerabilities. iRODS is designed to be run using an
+        // unprivileged system user account.
+        if (getuid() != geteuid()) {
+            fmt::print(stderr, "Warning: Real UID [{}] and effective UID [{}] must match. Exiting.\n", getuid(), geteuid());
             std::exit(1);
         }
-    } // terminate_if_launched_by_incorrect_system_user
+    } // terminate_if_launched_as_root
+
+            }
+
+            std::exit(1);
+        }
+        }
 
     auto validate_configuration() -> bool
     {
